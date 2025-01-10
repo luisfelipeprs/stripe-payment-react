2683e4993e094df63cbc9fc54213237ccc0c10d7
luisfelipeprs <feliperaxz@gmail.com>
2025-01-10 14:45:02 -0300

    feat: add Payment component

---
+++ src/app/components/Payment.tsx
@@ -0,0 +1,35 @@
+import { Elements } from "@stripe/react-stripe-js"
+import { useEffect, useState } from "react";
+import { loadStripe, Stripe } from '@stripe/stripe-js'
+import CheckoutForm from "./CheckoutForm";
+export default function Payment(){
+  const [stripePromise, setStripePromise] = useState<Promise<Stripe | null> | null>(null);
+  const [clientSecret, setClientSecret] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:5252/config").then(async (r) => {
+      const { publishableKey } = await r.json();
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:5252/create-payment-intent", {
+      method: "POST",
+      body: JSON.stringify({}),
+    }).then(async (result) => {
+      var { clientSecret } = await result.json();
+      setClientSecret(clientSecret);
+    });
+  }, []);
+
+  return (
+    <div className="">
+      <h1 className="text-red-400 font-bold text-2xl">Global System Care</h1>
+      {clientSecret && stripePromise && (
+        <Elements stripe={stripePromise} options={{ clientSecret }}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  )
+}
